a603675b40a6c7dfe47061b47665b2974c202ede
bonseno <tcutruongngocanh@gmail.com>
2025-06-13 14:55:06 +0700

    report

--- product-service/src/routes/report.routes.js
+++ product-service/src/routes/report.routes.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { StoreandStaffPerformanceReport } from "../controllers/report.controller.js";
+import {
+  StoreandStaffPerformanceReport,
+  TotalRevenueReport,
+} from "../controllers/report.controller.js";
 import { protectRoute } from "../middlerware/auth.middleware.js";
 
 const router = express.Router();
@@ -7,5 +10,6 @@ const router = express.Router();
 router.use(protectRoute);
 
 router.get("/StoreandStaffPerformanceReport", StoreandStaffPerformanceReport);
+router.get("/TotalRevenueReport", TotalRevenueReport);
 
 export default router;
