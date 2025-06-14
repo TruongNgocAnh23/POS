f53c4eb6a53eb96ae06ec7797df9a80e5b25371d
bonseno <tcutruongngocanh@gmail.com>
2025-06-14 10:49:21 +0700

    fix code

--- product-service/src/utils/generateCode.js
+++ product-service/src/utils/generateCode.js
@@ -8,9 +8,9 @@ function generateAutoCode(prefix = "HD") {
   const second = pad(now.getSeconds());
   const day = pad(now.getDate());
   const month = pad(now.getMonth() + 1); // tháng bắt đầu từ 0
-  const year = now.getFullYear();
+  const year = now.getFullYear().toString().slice(-2);
 
-  return `${prefix}${hour}${minute}${second}${day}${month}${year}`;
+  return `${prefix}${day}${month}${year}${hour}${minute}${second}`;
 }
 
 export default generateAutoCode;
